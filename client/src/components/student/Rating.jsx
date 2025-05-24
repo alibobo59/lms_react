4a77dd7bd6a603fb0dacc54b8fe1dc2a6d21b400
alibobo59 <nguyentamminh.ntm@gmail.com>
2025-05-25 01:35:28 +0700

    fix bug icon rating

--- client/src/components/student/Rating.jsx
+++ client/src/components/student/Rating.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
 
-const Rating = () => {
-  return <div>Rating</div>;
+const Rating = ({ initialRating, onRate }) => {
+  const [rating, setRating] = useState(0);
+
+  const handleRating = (value) => {
+    setRating(value);
+    if (onRate) {
+      onRate(value);
+    }
+  };
+
+  useEffect(() => {
+    if (initialRating) {
+      setRating(initialRating);
+    }
+  }, [initialRating]);
+  return (
+    <div>
+      {Array.from({ length: 5 }, (_, index) => {
+        const starValue = index + 1;
+        return (
+          <span
+            onClick={() => handleRating(starValue)}
+            key={index}
+            className={`text-xl sm:text-2xl cursor-pointer transition-color ${
+              starValue <= rating ? "text-yellow-400" : "text-gray-400"
+            }`}>
+            &#9733;
+          </span>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Rating;
