cf813cbfe7f3f3b385b3dbb78d41c0c173e23368
alibobo59 <nguyentamminh.ntm@gmail.com>
2025-05-25 08:04:57 +0700

    Test

--- client/src/components/educator/NavBar.jsx
+++ client/src/components/educator/NavBar.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { assets, dummyEducatorData } from "../../assets/assets";
+import { UserButton, UserBUtton, useUser } from "@clerk/clerk-react";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
-  return <div>Educator NavBar</div>;
+  return (
+    <div>
+      <Link to="/">
+        {" "}
+        <img src={assets.logo} alt="Logo" className="w-28 lg:w-32" />
+      </Link>
+      <div>
+        <p>Hi {user ? user.fullName : "User"}</p>
+        {user ? <UserButton /> : <img className="" />}
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
